5af55e0e5f834f66be18e5329e30cdc4d3171c15
Fernando Henrique <fernandohtr@gmail.com>
2019-07-18 23:32:15 -0300

    Salva informações de questionário no json

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import os
+import json
 
 
 def app(environ, start_response):
-    print(environ['wsgi.input'].read())
     url_path = environ['PATH_INFO']
+    read_bytes = environ['wsgi.input'].read()
+
+    if len(read_bytes) > 0:
+        with open('content/register.txt', 'a') as save_doc:
+            read_str = read_bytes.decode('utf-8')
+            save_doc.write(read_str)
 
     if url_path == '/' or url_path == '/static/':
         url_path = '/index.html'
 
     if url_path[1:] in os.listdir('static'):
-        response = route(url_path)
+        response = route(environ, url_path)
     else:
         response = not_found()
 
@@ -25,11 +31,26 @@ def not_found():
     return body, headers, status
 
 
-def route(url_path):
+def route(environ, url_path):
+    if environ['REQUEST_METHOD'] == 'POST':
+        read_bytes = environ['wsgi.input'].read()
+        read_str = read_bytes.decode('utf-8')
+        with open('content/register.json', 'w') as f:
+            f.write(read_str)
+        save_info(read_str)
+
     body = read_body(url_path)
     headers = [('Content-Type', mime(url_path))]
     status = '200 OK'
     return body, headers, status
+
+
+def save_info(read_str):
+    infos = read_str.split('&')
+    for i in range(3):
+        key, valor = infos[i].split('=')
+        info_organize[key] = valor
+    json.dumps(info_organize)
 
 
 def read_body(url_path):
